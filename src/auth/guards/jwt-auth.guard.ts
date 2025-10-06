8f62606c6a1a753cbbae27e2046fdde734371f08
contactdavinciera <236133761+contactdavinciera@users.noreply.github.com>
2025-10-06 13:43:31 -0400

    Fix: Refine Request typing in jwt-auth.guard.ts

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,10 @@
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest<Request>();
+    const request = context.switchToHttp().getRequest();
     if (request.method === 'OPTIONS') {
       return true;
     }
